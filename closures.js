b1e466a6f1c3dd1bd9c3bb27b71a6c77dff77646
Marty Kowbel <mekobes44@yahoo.com>
2015-10-23 17:15:11 -0500

    day1 closures

--- closures.js
+++ closures.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer()
 
 //Once you do that, invoke inner.
 
-  //Code Here
+inner()
 
 
 
@@ -32,8 +32,19 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
-
+var callFriend = function(){
+  var friend = 'Jake';
+  function callF(number){
+    return 'Calling ' + friend + ' at ' + number;
+  }
+  return callF;
+};
+myCall = callFriend()
+callF(number){
+    return 'Calling ' + friend + ' at ' + number;
+  }
+myCall("435-215-9248")
+"Calling Jake at 435-215-9248"
 
 
 //Next Problem
@@ -44,8 +55,18 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+
+  function makeCounter() {
+      var i = 0;
+     
+      return function() { 
+            i++;
+          return i;
+      }
+         
+  }
   var count = makeCounter();
+
   count() // 1
   count() // 2
   count() // 3
@@ -60,9 +81,23 @@ var callFriend = function(){
 /*
   Write a function that does something simple (console, alert, etc). Write a second function that accepts the first function as it's first parameter. The second function should return a new third function which, when invoked, invokes the first, original function that was passed in, but will only ever do so once.
 */
-
-  //Code Here
-
+function marty(a) {
+    alert(a);
+}
+function kobes(cb) {
+    var flag = false;
+    return function(a) {
+        if(!flag){
+            cb(a);
+            flag = true;
+        } else {
+            console.log('done');
+        };
+    };
+};
+  
+var test = kobes(marty);
+test();
 
 
 //Next Problem
@@ -72,8 +107,35 @@ var callFriend = function(){
 /*
   Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
 */
+var test = function () {
+    alert("hi");
+}
+function fnCounter(func,n) {
+    test = func;
+    for (var i =1; i <= n; i++) {
+       func();
+    }
+    
+    return "STOP";
+}
 
 
+function marty(a) {
+    alert(a);
+}
+function kobes(cb, n) {
+    var counter = 0;
+    return function(a) {
+        if(counter <= n){
+            cb(a);
+            counter++
+        } else {
+            console.log('done');
+        };
+    };
+};
+  
+var test = kobes(marty, 3);
 
 //Next Problem
 
